60cd2ffb41f13bb0ff13196abab4bb6728b5d932
Weverton Couto <wevertoncristais@gmail.com>
2020-10-27 09:36:16 -0300

    Website

---
+++ src/resumeData.js
@@ -0,0 +1,90 @@
+let resumeData = {
+    "imagebaseurl":"https://avatars0.githubusercontent.com/u/35855799?s=460&u=ec54d49bea0c74da56848bee728cbca1b032d5b7&v=4",
+    "name": "Weverton Couto",
+    "role": "Web Developer and Mobile Developers",
+    "linkedinId":"wevertoncouto",
+    "roleDescription": "I like dabbling in various parts of web/mobile development and like to learn about new technologies, in my free time I like to play tycoon games.",
+    "socialLinks":[
+        {
+          "name":"linkedin",
+          "url":"https://www.linkedin.com/in/wevertoncouto/",
+          "className":"fa fa-linkedin"
+        },
+        {
+          "name":"github",
+          "url":"https://github.com/WevertonCouto",
+          "className":"fa fa-github"
+        },
+      ],
+    "aboutme":"I am currently in 6 semester at Wyden Unimetrocamp. I am a self taught Web Developer, currently diving deeper into Mobile Developer. I believe that to be successful in life, one needs to be obsessive with their dreams and keep working towards them.",
+    "address":"Brazil",
+    "website":"https://github.com/WevertonCouto",
+    "education":[
+      {
+        "UniversityName":"Wyden Unimetrocamp",
+        "specialization":"Science of Computer",
+        "MonthOfPassing":"January",
+        "YearOfPassing":"2018",
+      }
+    ],
+    "work":[
+      {
+        "CompanyName":"Golden Skill It Solutions",
+        "specialization":"Some specialization",
+        "MonthOfLeaving":"August",
+        "YearOfLeaving":"2018",
+      },
+    ],
+    "skillsDescription":"Coming soon",
+    "skills":[
+      // {
+      //   "skillname":"HTML5"
+      // },
+      // {
+      //   "skillname":"CSS"
+      // },
+      // {
+      //   "skillname":"Reactjs"
+      // },
+      // {
+      //   "skillname":"Jquery"
+      // },
+      // {
+      //   "skillname":"c#"
+      // }
+    ],
+    "portfolio":[
+      {
+        "name":"gsDoctor",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/gsdoctor.png"
+      },
+      {
+        "name":"Reproferty",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/reproferty.png"
+      },
+      {
+        "name":"Originare",
+        "description":"mobileapp",  
+        "imgurl":"images/portfolio/originare.png"
+      },
+      {
+        "name":"Fertilidade e Vida",
+        "description":"mobileapp",
+        "imgurl":"images/portfolio/fertilidaevida.png"
+      }
+    ],
+    "testimonials":[
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // },
+      // {
+      //   "description":"This is a sample testimonial",
+      //   "name":"Some technical guy"
+      // }
+    ]
+  }
+  
+  export default resumeData
